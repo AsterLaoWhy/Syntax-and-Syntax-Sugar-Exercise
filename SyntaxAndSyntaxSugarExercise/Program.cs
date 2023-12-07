f25680d2f75cadf516e8ef97e42415b77eee3344
Jon <jonathon_rylatt@yahoo.com>
2023-12-07 18:55:30 +0800

    Reduced the code to 2 lines

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -3,8 +3,10 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {  
+            var answer = 4;
+            var response = answer < 9 ? $"{answer} is less than nine":$"{answer } is greater than or equal to nine";
+            Console.WriteLine(response);
         }
     }
 }
